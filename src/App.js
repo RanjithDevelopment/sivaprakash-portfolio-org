4db53a82a49afeed8eae918ad384a39c93cac9a5
RanjithDevelopment <ranjithproffesional@gmail.com>
2023-01-21 12:00:49 +0530

    siva portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Home from './Routes/Home';
+import Project from './Routes/project';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+import {Route,Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+
+      <Route path="/" element={<Home/>} />
+      <Route path="/Project" element={<Project/>} />
+      <Route path="/About" element={<About/>} />
+      <Route path="/Contact" element={<Contact/>} />
+
+      
+    </Routes>
     </div>
   );
 }
